bed7b6a2dc98743a0d98addc1881553294b501f3
shaiksha7 <shaiksha1431813@gmail.com>
2024-04-19 10:45:15 +0530

    add javascript

---
+++ javascript.js
@@ -0,0 +1,21 @@
+const textbox=document.getElementById(`textbox`);
+toFahrenheit=document.getElementById(`toFahrenheit`)
+tocelsius=document.getElementById(`tocelsius`)
+result=document.getElementById(`result`);
+let temp;
+function convert(){
+if(toFahrenheit.checked){
+   temp=Number(textbox.value)
+   temp=temp * 9/5 + 32;
+   result.textContent=temp.toFixed(1)+"℉"
+}
+else if(tocelsius.checked){
+    temp=Number(textbox.value)
+    temp=(temp-32) *5/9;
+    result.textContent=temp.toFixed(1)+"℃"
+ }
+else{
+    result.textContent= `Select a unit`;
+}
+
+}
